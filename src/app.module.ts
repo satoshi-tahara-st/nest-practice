a35718186c53b96ab467819cb4bec9943bd8afd4
satoshi-tahara-st <sts3104mnr@gmail.com>
2023-10-02 23:30:21 +0900

    DB途中

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-// import {TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [ItemsModule, TypeOrmModule.forRoot()],
   controllers: [],
   providers: [],
 })
