7754368b634306fc1f178ec5e485eec1bf9eef33
satoshi-tahara-st <sts3104mnr@gmail.com>
2023-10-04 21:51:01 +0900

    sign up apiの実装

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { User } from 'src/entities/user.entitiy';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.authService.signUp(createUserDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entitiy';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async signUp(createUserDto: CreateUserDto): Promise<User> {
+    const { username, password, status } = createUserDto;
+    const user = this.userRepository.create({
+      username,
+      password,
+      status,
+    });
+    await this.userRepository.save(user);
+    return user;
+  }
+}
